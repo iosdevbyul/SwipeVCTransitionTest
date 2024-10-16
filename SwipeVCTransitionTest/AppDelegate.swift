1dd144e00a01eada250b41d73f15896625518ab6
comatoki <hbabnd@gmail.com>
2024-10-17 02:17:35 +0900

    feat Add RealmManager

--- SwipeVCTransitionTest/AppDelegate.swift
+++ SwipeVCTransitionTest/AppDelegate.swift
@@ -11,12 +11,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        window = UIWindow(frame: UIScreen.main.bounds)
-//        let tabBarController = MainViewController()
-//        tabBarController.viewControllers = [FirstViewController(), SecondViewController(), ThirdViewController()]
-//        window?.rootViewController = tabBarController
-//        window?.makeKeyAndVisible()
-        print("a")
         return true
     }
 
---
+++ SwipeVCTransitionTest/UI/Manager/RealmManager.swift
@@ -0,0 +1,8 @@
+//
+//  RealmManager.swift
+//  SwipeVCTransitionTest
+//
+//  Created by COMATOKI on 2024-10-17.
+//
+
+import Foundation
