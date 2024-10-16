26b39a561bb425776e3010ae66fead16c68a5e13
comatoki <hbabnd@gmail.com>
2024-10-16 11:45:53 +0900

    Initial Commit

---
+++ SwipeVCTransitionTestTests/SwipeVCTransitionTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeVCTransitionTestTests.swift
+//  SwipeVCTransitionTestTests
+//
+//  Created by COMATOKI on 2024-10-16.
+//
+
+import Testing
+@testable import SwipeVCTransitionTest
+
+struct SwipeVCTransitionTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
